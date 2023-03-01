338b9bc660ef935603a3836ae72dd692bf95835c
Ítalo César Silva Oliveira <italo.cs.oliveira@gmail.com>
2023-03-01 15:45:02 -0300

    :sparkles: Iniciando Projeto :sparkles:

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { IoLogoGameControllerB } from 'react-icons/io';  
+import './styles.css';
+
+  function App() {
+
+    function name(params) {
+      
+    }
+
+    return (
+      <div className="container">
+        <h1 className="title"> Jogo do Nim </h1>
+        <button className="button" >
+          <IoLogoGameControllerB size={40} color="#333333"/>
+        </button>
+      </div>
+    );
+  }
+
+export default App;
